3eee37583b82c5317bbf987af75d1881093bb6a4
MahadevT <mahadevlt02@gmail.com>
2022-12-27 15:49:08 +0530

    reverting back to correct code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
-//public class TestGreeter {
+public class TestGreeter {
 
   private Greeter greeter;
 
